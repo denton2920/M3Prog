6f9b79140eb875a31d5ceed72c8de60948eb777c
Dennis <37660@ma-web.nl>
2024-03-08 09:48:32 +0100

    02variabelen

---
+++ m3prog_project/public/02/variabelen.php
@@ -0,0 +1,25 @@
+<?php
+//$auto's	= "mini";
+//$auto/onderdelen	= "uitlaat";
+//$huis&tuin	= "bezem";
+//$22	= "jaar";
+$kleurVanAuto =	"blauw";
+//$software-dev = "geen wat ik hier moet schrijven";
+$auto2 ="BMW";
+echo $auto2;
+
+
+$_naam	= "Dennis";
+echo $_naam;
+
+$naw = "?";
+echo $naw;
+
+$auto ="Daf";
+echo $auto;
+
+
+echo $kleurVanAuto;
+
+?>
+
